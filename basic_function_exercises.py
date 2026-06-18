185d2ce35d5e57630f07c097f637edde9b8af587
NayraCeleste <celesteroma2010@gmail.com>
2026-06-17 21:35:19 -0600

    Practice Python functions and return values

---
+++ basic_function_exercises.py
@@ -0,0 +1,64 @@
+# void no nos regresa nada
+
+
+""" def suma():
+    numero1= int(input("Ingresa un número "))
+    numero2= int(input("Ingresa un número "))
+    return numero1 + numero2
+
+def respuesta():
+    suma_ = suma()
+    print(f"el resultado es {suma_}") """
+
+#area circulo 
+""" def Areacirculo():
+    radio= float(input("cual es el radio?"))
+    return 3.1416 * (radio* radio)
+
+def resultado()
+    Areacirculo_ = Areacirculo()
+    print(f"el resultado es {Areacirculo_}")
+
+resultado() """
+
+venden dulces cada dulce 8 pesos +10 5+ 20 12 PESOS
+
+=10 5 persos 
+=+20 12
+
+
+SACAR EL COSTO,
+TODAL
+DESCUENTO EL COSTO, 
+A PAGAR CUANTO ME DAN , 
+MOSTRAR EL CAMBIO  
+
+def quieres():
+numDeDulces=int(input("cuantos dulces compraste? "))
+
+def costo():
+    return numDeDulces*8 
+
+def descuento():
+    costo_=costo()
+
+    if numDeDulces == 20:
+        costo_ = costo_- 12 
+    if numDeDulces == 10:
+        costo_ = costo_ - 5
+    
+    print(f"el costo inicial es de {costo()} con descuento es {costo_}")
+
+def cambio(costo_):      
+    dineroRecibido=int(input("recibo "))
+    cambio = dineroRecibido - costo_
+
+def inteDeCambio(costo_):
+    cambio_= cambio(costo_)
+    print("tu cambio es de {cambio_}")
+
+
+
+descuento()
+
+inteDeCambio() 
