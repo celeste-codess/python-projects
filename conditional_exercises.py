77193a6dfae01bc4982cba6d9b3d88390ee93991
NayraCeleste <celesteroma2010@gmail.com>
2026-06-17 21:28:48 -0600

    Practice Python conditionals and boolean logic

---
+++ conditional_exercises.py
@@ -0,0 +1,97 @@
+""" temperatura = float(input("Ingresa tu temperatura "))
+
+
+
+def fiebre(temperatura):
+    if temperatura >= 38:
+        print("Tienes fiebre.")
+    if temperatura >= 36 or temperatura <=37:
+        print("Estas normal.")
+    else:
+        print("No tienes fiebre.")
+
+fiebre(temperatura) """
+
+
+""" calificacion = float(input("ingresa tu calificación"))
+
+def pase_o_no(calificacion):
+    if calificacion >= 7 and calificacion<=9 :
+        print("pasaste")
+    if calificacion <= 6:
+        print("reprobaste")
+    else:
+        print("exentaste")
+
+pase_o_no(calificacion) """
+
+#medicamento
+""" medicamento= (input("que medicamento "))
+
+def medicina(medicamento):
+    if medicamento == "paracetamol":
+     print("te vas a moriiir 😵") 
+    else:
+        print("sobreviviste ")
+
+medicina(medicamento)
+
+ """
+#mayonesa o cabsu
+
+""" tiene= (input("que tiene? "))
+
+
+si_mayonesa="mayonesa" in tiene
+si_cabsu="cabsu" in tiene
+
+def gusta (mayonesa, cabsu):
+    if mayonesa==True or cabsu==True:
+       print("no come") 
+    else:
+        print("si come")
+
+gusta(si_mayonesa, si_cabsu)
+
+ """
+#baila o no 
+""" 
+baila=(input("que bailan? " ))
+
+si_baile= "hombres de negro" in baila
+
+def participo_si_no(hombres):
+    if hombres== True:
+        print("si baila")
+    else:
+        print("no baila")
+
+participo_si_no(si_baile) """
+
+#paso o no
+
+""" calificacion = float(input("ingresa tu calificación "))
+
+def pase_o_no(calificacion):
+    if calificacion >=7 and calificacion <=8 :
+        print("pasaste")
+    if calificacion >= 9:
+        print("puedes exentar yeiii")
+    if calificacion < 7:
+        print("reprobaste")
+   
+pase_o_no(calificacion) """
+
+#mayonesa
+""" tiene= (input("que tiene? "))
+
+si_mayonesa="mayonesa"
+si_cabsu="cabsu" 
+
+def gusta (mayonesa, cabsu):
+    if mayonesa==True or cabsu==True:
+       print("no come") 
+    else:
+       print("si come")
+
+gusta(si_mayonesa, si_cabsu) """
